d690e8a072fc966558029263b458311ec405abfc
Rachel Bock <rachel.online@gmail.com>
2022-04-08 17:11:34 -0600

    Feat: Add prepShouldCook functionality

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var cookPotImage = document.querySelector('.pot');
 
 // ~~~~~~~~~~~ Variables ~~~~~~~~~~~~~~~~~~
 var desserts = [
+    'Iced Oatmeal Cookies',
     'Apple Pie',
     'Lemon Meringue Pie',
     'Black Forest Cake',
@@ -25,6 +26,7 @@ var desserts = [
 ]
 
 var mains = [
+    'Chicken Vindaloo',
     'Spaghetti and Meatballs',
     'Pineapple Chicken',
     'Shakshuka',
@@ -40,9 +42,8 @@ var mains = [
     'Margarita Pizza'
 ]
 
-// var shouldCook = '';
-
 var sides = [
+    'Soft Pretzels'
     'Miso Glazed Carrots',
     'Coleslaw',
     'Garden Salad',
@@ -61,22 +62,26 @@ letsCookButton.addEventListener('click', getUserOption);
 
 // ~~~~~~~~~~ Functions in Alphabetic order ~~~~~~~~~~~~~
 
-// function - select random index for side, main dish, or dessert
+// function - pull random meal item from array into render.
+function getDish(dishArray) {
+    return dishArray[getRandomIndex(dishArray)];
+}
 
+
+// function - select random index for side, main dish, or dessert
+function getRandomIndex(dishArray) {
+    return Math.floor(Math.random * dishArray.length);
+}
 
 
 function getUserOption() {
     // When the "Let's cook!" button is clicked, this 
     // gets the user's option from the radio button inputs.
     // If entire meal, run render meal instead of render().
+
+    
+
 }
-
-
-
-// function - pull random meal item from array into render.
-
-// function - prepare data model shouldCook for rendering.
-
 
 
 // function - change class hidden on pot.
@@ -84,6 +89,29 @@ function hideCookPot() {
     cookPotImage.classList.toggle('hidden');
 }
 
+// function - prepare data model for rendering.
+function prepShouldCook(dishType) {
+
+    hideCookPot();
+
+    switch (dishType) {
+        case 'side':
+            render(getDish(sides));
+            break;
+        case 'main':        
+            render(getDish(mains));
+            break;
+        case 'dessert':
+            render(getDish(desserts));
+            break;
+        case 'meal':
+            console.log('Call getDish for all three and then call the meal render function');
+            render(getDish(sides), getDish(mains), getDish(desserts));
+            break;
+        default: 
+            console.log(`Sorry.  DishType is unknown.`);
+    }
+}
 
 // function - render results.
 function render(shouldCook) {   
